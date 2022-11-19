b54f2bfb3701b22265ed970378439bc56e4f3f81
Aizhan-15 <aizhanurman@gmail.com>
2022-11-20 02:16:25 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,10 +2,9 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint) {
-        int j;
-        for (numberTableToPrint = 1; numberTableToPrint <= 10; numberTableToPrint++) {
-            for (j = 1; numberTableToPrint <= 10; numberTableToPrint++) {
-                System.out.print(numberTableToPrint * j + " ");
+        for (int i = 1; i <= numberTableToPrint; i++) {
+            for (int j = 1; j <= numberTableToPrint; j++) {
+                System.out.print(i*j + " ");
             }
             System.out.println();
         }
