d8c23d344a234e38e0aaf031cb80881dbfa832ac
Aizhan-15 <aizhanurman@gmail.com>
2022-11-20 02:07:23 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,10 +2,17 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 1; j < cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
 
-    }
-
-    public static void main(String[] args) {
-        new HalfPyramid().printHalfPyramid(8);
+            public static void main(String[]args){
+                new HalfPyramid().printHalfPyramid(8);
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,13 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+    public void printTable(int numberTableToPrint) {
+        int j;
+        for (numberTableToPrint = 1; numberTableToPrint <= 10; numberTableToPrint++) {
+            for (j = 1; numberTableToPrint <= 10; numberTableToPrint++) {
+                System.out.print(numberTableToPrint * j + " ");
+            }
+            System.out.println();
+        }
     }
 }
