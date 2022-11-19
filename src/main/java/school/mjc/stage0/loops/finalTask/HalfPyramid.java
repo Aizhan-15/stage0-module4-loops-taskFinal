acbb1a54a8192a53c7dbf4bad7114d737db02eef
Aizhan-15 <aizhanurman@gmail.com>
2022-11-20 02:36:32 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -5,13 +5,12 @@ public class HalfPyramid {
         for (int i = 0; i < cathetusLength; i++) {
             for (int j = 1; j < cathetusLength - i; j++) {
                 System.out.print(" ");
+                for (int k = 0; k <= i; k++) {
+                    System.out.print("*");
+                }
             }
-            for (int k = 0; k <= i; k++) {
-                System.out.print("*");
-            }
-    }
-    }
-            public static void main(String[] args){
+        }
+    }   public static void main(String[] args){
                 new HalfPyramid().printHalfPyramid(8);
 
             }
